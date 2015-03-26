a41020ca0d9664b308112139a26a1ebf29a725fa
Ross McIlwaine <RossMcIlwaine@Rosss-MacBook-Air.local>
2015-03-26 09:38:54 -0600

    Creates Xcode Project

---
+++ iosCompetencyTest/iosCompetencyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosCompetencyTest
+//
+//  Created by Ross McIlwaine on 3/26/15.
+//  Copyright (c) 2015 Ross McIlwaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iosCompetencyTest/iosCompetencyTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iosCompetencyTest
+//
+//  Created by Ross McIlwaine on 3/26/15.
+//  Copyright (c) 2015 Ross McIlwaine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
